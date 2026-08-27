0fa8935bee71cde005dfaccf68cdd38563b9e705
Michael Adam <obnox@samba.org>
2026-08-27 20:25:02 +0200

    sqstack
    Signed-off-by: Michael Adam <obnox@samba.org>

--- containerbox/stack.py
+++ containerbox/stack.py
@@ -1,5 +1,6 @@
 from containerbox import Chain
 
+
 class Stack:
     def __init__(self):
         self._chain = Chain()
@@ -9,10 +10,12 @@ class Stack:
 
     def pop(self):
         return self._chain.remove_at(0)
+
     def __len__(self):
         return len(self._chain)
+
     def __bool(self):
         return len(self) > 0
+
     def peek(self):
         return self._chain.get_head()
-
