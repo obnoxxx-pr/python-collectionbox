9c93e35dd0e12563a3c573c49f0110f5351e7454
Michael Adam <obnox@samba.org>
2026-08-28 13:41:04 +0200

    queue: add Queue implementation with tests and demo
    Signed-off-by: Michael Adam <obnox@samba.org>

--- containerbox/queue.py
+++ containerbox/queue.py
@@ -10,3 +10,12 @@ class Queue:
 
     def dequeue(self):
         return self._chain.remove_at(0)
+
+    def __len__(self):
+        return len(self._chain)
+
+    def __repr__(self):
+        return f"Queue({list(self._chain)})"
+
+    def __bool__(self):
+        return len(self) > 0
